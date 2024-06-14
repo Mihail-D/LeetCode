0140d44ede88ffd79e3f75b9b90d32dbd6bd2c3c
Nuby <nuby.4register@gmail.com>
2024-06-14 11:36:31 +0300

    LengthOfLastWord

---
+++ Easy/LengthOfLastWord.java
@@ -0,0 +1,16 @@
+package Easy;
+
+public class LengthOfLastWord {
+
+    public int lengthOfLastWord(String s) {
+        String[] arr = s.trim().split(" ");
+
+        return arr[arr.length - 1].length();
+    }
+
+    public static void main(String[] args) {
+        LengthOfLastWord sol = new LengthOfLastWord();
+        String s = "   fly me   to   the moon  ";
+        System.out.println(sol.lengthOfLastWord(s));
+    }
+}
