2a9e0b4f12ae16f785d26b2a2f80733479d36ab7
Nuby <nuby.4register@gmail.com>
2024-06-21 11:55:08 +0300

    DeleteDuplicates

---
+++ Easy/DeleteDuplicates.java
@@ -0,0 +1,52 @@
+package Easy;
+
+public class DeleteDuplicates {
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(3);
+        head.next = node1;
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+
+        ListNode result = deleteDuplicates(head);
+        while (result != null) {
+            System.out.println(result.val);
+            result = result.next;
+        }
+    }
+
+    public static ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+
+        ListNode current = head;
+        while (current.next != null) {
+            if (current.val == current.next.val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+
+        return head;
+    }
+
+    public static class ListNode {
+
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+}
+
+// https://leetcode.com/problems/remove-duplicates-from-sorted-list/
