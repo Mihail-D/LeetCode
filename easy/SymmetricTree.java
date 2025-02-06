3517b588e57ab098dbeb6f76119e9bc184006c63
Nuby <nuby.4register@gmail.com>
2025-02-06 14:28:12 +0300

    BinarySearch

---
+++ easy/SymmetricTree.java
@@ -0,0 +1,33 @@
+package easy;
+
+public class SymmetricTree {
+//Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+
+    public boolean isSymmetric(TreeNode root) {
+        return isMirror(root, root);
+    }
+
+    public boolean isMirror(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) {
+            return true;
+        }
+        if (t1 == null || t2 == null) {
+            return false;
+        }
+
+        return (t1.val == t2.val) && isMirror(t1.left, t2.right) && isMirror(t1.right, t2.left);
+    }
+
+    public static void main(String[] args) {
+        SymmetricTree st = new SymmetricTree();
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(2);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(4);
+        root.right.left = new TreeNode(4);
+        root.right.right = new TreeNode(3);
+
+        System.out.println(st.isSymmetric(root));
+    }
+}
