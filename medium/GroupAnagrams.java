f3586fa52c32fc249fb877052c841b696be76772
Nuby <nuby.4register@gmail.com>
2025-02-07 22:55:25 +0300

    GroupAnagrams

---
+++ medium/GroupAnagrams.java
@@ -0,0 +1,17 @@
+package medium;
+
+import java.util.List;
+
+public class GroupAnagrams {
+
+    public static List<List<String>> groupAnagrams(String[] strs) {
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(groupAnagrams(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"}));
+        System.out.println(groupAnagrams(new String[]{"eat"}));
+        System.out.println(groupAnagrams(new String[]{}));
+    }
+
+}
